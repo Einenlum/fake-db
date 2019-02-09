60211a7db3385c6649b4158dea8eab2b9d1f6f8b
Einenlum <yann.rabiller@gmail.com>
2019-02-09 20:40:52 +0100

    Move check_value_integrity to a dedicated method

--- src/field.py
+++ src/field.py
@@ -1,8 +1,10 @@
 class Field:
-    allowed_types = ['str', 'int', 'bool']
+
+    ALLOWED_TYPES = ['str', 'int', 'bool']
+
     def __init__(self, name: str, type: str, **options):
-        if type not in Field.allowed_types:
-            raise Exception(f'{type} is not a valid type. Valid types: {" ".join(Field.allowed_types)}.')
+        self.__check_type_integrity(type)
+
         self.name = name
         self.type = type
 
@@ -24,3 +26,8 @@ class Field:
         for type_name, obj in types.items():
             if self.type == type_name and not isinstance(value, obj):
                 raise Exception(f"The value should be of type {value} but it is { type(value) }")
+
+    def __check_type_integrity(self, type):
+        if type not in Field.ALLOWED_TYPES:
+            raise Exception(f'{type} is not a valid type. Valid types: {" ".join(Field.ALLOWED_TYPES)}.')
+
