4be8267da0622f9206ad95ad2a8c76cc64ced4f0
Einenlum <yann.rabiller@gmail.com>
2019-02-10 18:34:47 +0100

    Add mocks in unit tests

--- tests/db.py
+++ tests/db.py
@@ -1,49 +1,42 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from src.db import Db
 from src.table import Table
 from src.field import Field
 
 class TestDb(TestCase):
     def test_instanciation(self):
-        table = Table('users', [
-            Field('id', 'int', primary_key=True),
-            Field('name', 'str'),
-            Field('age', 'int', nullable=True)
-        ])
+        table = mock.Mock(spec=Table)
 
         db = Db([table])
 
     def test_insert(self):
-        table = Table('users', [
-            Field('id', 'int', primary_key=True),
-            Field('name', 'str'),
-            Field('age', 'int', nullable=True)
-        ])
+        table = mock.Mock(spec=Table)
+        table.name = 'users'
 
         db = Db([table])
         db.insert('users', id=2, name='Roger')
+        table.insert.assert_called_once_with(id=2, name='Roger')
 
     def test_exception_if_table_does_not_exist(self):
-        table = Table('users', [
-            Field('id', 'int', primary_key=True)
-        ])
+        table = mock.Mock(spec=Table)
+        table.name = 'users'
 
         db = Db([table])
         with self.assertRaises(Exception):
             db.insert('despacito', id=2)
 
     def test_export(self):
-        table = Table('users', [
-            Field('id', 'int', primary_key=True),
-            Field('name', 'str')
-        ])
+        table = mock.Mock(spec=Table)
+        table.name = 'users'
+        table.export.return_value = [
+            {'id': 1, 'name': 'Roger'},
+            {'id': 2, 'name': 'Patrick'}
+        ]
 
         db = Db([table])
-        db.insert('users', id=1, name='Roger')
-        db.insert('users', id=2, name='Pierre')
         self.assertEqual(db.export(), [
             {'users': [
                 {'id': 1, 'name': 'Roger'},
-                {'id': 2, 'name': 'Pierre'}
+                {'id': 2, 'name': 'Patrick'}
             ]}
         ])
